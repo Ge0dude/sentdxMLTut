47efcfc3e2e55bf94e0ef7f644a8b38e4f439e95
Brendon <btucker1992@gmail.com>
2017-05-07 06:55:30 +0800

    plt stlyes are actually quite nice, don't know why everyone insists on awful graphs in python...

--- MonteCarlo/video1.py
+++ MonteCarlo/video1.py
@@ -7,39 +7,52 @@ Created on Sat May  6 16:52:05 2017
 """
 
 import random 
+import matplotlib
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
 
 def rolldice():
     roll = random.randint(1,100)
     if roll == 100:
-        print (roll) 
-        print( 'You lost')
+#        print (roll) 
+#        print( 'You lost')
         return False
     elif roll <= 50:
-        print (roll) 
-        print ('You lost')
+#        print (roll) 
+#        print ('You lost')
         return False
     elif 100 > roll >= 51:
-        print (roll) 
-        print ('You win')
+#        print (roll) 
+#        print ('You win')
         return True
         
 def simpleBettor(funds, initial_wager, wager_count):
     value = funds
     wager = initial_wager
     
-    currentWager = 0
+    wX = []
+    vY = []
     
-    while currentWager < wager_count:
+    currentWager = 1
+    
+    while currentWager <= wager_count:
         if rolldice(): #if True is resturned this will execute
             value = value + wager
+            wX.append(currentWager)
+            vY.append(value)
         else:
-            value = value - wager  #value =- wager why didn't that work, why
+            value = value - wager  #value -= wager why didn't that work, why
             #did I have to write it out?
+            wX.append(currentWager)
+            vY.append(value)
+            
             
         currentWager += 1
-        print('Funds:', value)
+    #print('Funds:', value)
+    plt.plot(wX, vY)
         
-simpleBettor(10000, 100, 100)
+for x in range(100):
+    simpleBettor(10000, 100, 100)
        
         
         
